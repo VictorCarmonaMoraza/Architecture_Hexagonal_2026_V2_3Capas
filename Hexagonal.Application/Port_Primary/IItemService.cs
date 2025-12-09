fa959efcfdf588e9209c400615006178762efb7c
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-09 22:02:50 +0100

    Creacion de Puerto Primario IService(lo nombre como  IItemService)

---
+++ Hexagonal.Application/Port_Primary/IItemService.cs
@@ -0,0 +1,10 @@
+﻿using Hexagonal.Domain.Entities;
+
+namespace Hexagonal.Application.Port_Primary
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<Item>> GetAllItemsAsync();
+        Task AddItemAsync(string title);
+    }
+}
