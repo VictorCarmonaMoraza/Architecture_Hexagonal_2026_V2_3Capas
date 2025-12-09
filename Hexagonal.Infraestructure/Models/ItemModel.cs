6eebdb0a4c4fd9c3572d473afd1cf5fe72a1fd17
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-09 22:38:24 +0100

    Creacion de Model ItemModel

---
+++ Hexagonal.Infraestructure/Models/ItemModel.cs
@@ -0,0 +1,11 @@
+﻿//Tabla de la base de datos
+namespace Hexagonal.Infraestructure.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
