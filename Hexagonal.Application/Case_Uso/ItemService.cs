beb82859bd9d9be157bf6cee1b30e9e9538d3b57
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-09 22:10:33 +0100

    Implementacion Puerto Primario(caso de uso,Servicio)

---
+++ Hexagonal.Application/Case_Uso/ItemService.cs
@@ -0,0 +1,29 @@
+﻿using Hexagonal.Application.Port_Primary;
+using Hexagonal.Domain.Entities;
+using Hexagonal.Domain.interfaces;
+
+namespace Hexagonal.Application.Case_Uso
+{
+    public class ItemService : IItemService
+    {
+        private readonly IItemRepository _itemRepository;
+
+        public ItemService(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        public async Task AddItemAsync(string title)
+        {
+            //Creamos un nuevo item con el id 0 (se asignará automáticamente), el título proporcionado y el estado incompleto (false)
+            var item = new Item(0, title, false);
+            //Llamamos al repositorio para agregar el nuevo item
+            await _itemRepository.AddAsync(item);
+        }
+
+        public async Task<IEnumerable<Item>> GetAllItemsAsync()
+        {
+            return await _itemRepository.GetAllItemsAsync();
+        }
+    }
+}
