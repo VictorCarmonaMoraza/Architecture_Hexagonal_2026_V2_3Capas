c42aa0869f1778db7ae53bb875115c5b55167da8
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-10 12:34:30 +0100

    Creacion del context de Entity Framework

---
+++ Hexagonal.Infraestructure/DBContext/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using Hexagonal.Infraestructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hexagonal.Infraestructure.DBContext
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<ItemModel> ItemsModel { get; set; }
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Mapeamos nuestro modelo contra la tabla de la base de datos
+            modelBuilder.Entity<ItemModel>().ToTable("Items");
+        }
+    }
+}
