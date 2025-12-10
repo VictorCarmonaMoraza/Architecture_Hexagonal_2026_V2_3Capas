7180bc03d8e57147b3c4690536d84fdfd9cac383
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-10 19:54:20 +0100

    Inyeccion de dependecnias en el API

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,10 @@
+using Hexagonal.Application.Case_Uso;
+using Hexagonal.Application.Port_Primary;
+using Hexagonal.Domain.interfaces;
+using Hexagonal.Infraestructure.DBContext;
+using Hexagonal.Infraestructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +13,16 @@ builder.Services.AddOpenApi();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+//Inyeccion de dependencias
+builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(connectionString));
+builder.Services.AddTransient<IItemRepository,ItemRepository>();
+builder.Services.AddTransient<IItemService,ItemService>();
+//Fin inyeccion de dependencias
+
 //Configurar swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Fin Swagger
 
 var app = builder.Build();
 
