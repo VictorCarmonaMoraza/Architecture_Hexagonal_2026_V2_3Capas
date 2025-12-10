969ca48059c549aa3eb0d3a828d8b65f13ffecbb
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-10 13:37:53 +0100

    Creacion de archivo de Configuracion

--- Proyecto_De_Consola/Program.cs
+++ Proyecto_De_Consola/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional:false,reloadOnChange:true)
+    .Build();
+
+string connectionString = configuration.GetConnectionString("DefaultConnection");
