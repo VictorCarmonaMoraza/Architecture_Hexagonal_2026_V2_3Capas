52a4540556c5e5ea3415c281cbbcb6aa7403d456
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-10 13:45:18 +0100

    Inyecccion de dependencia de las implementaciones

--- Proyecto_De_Consola/Program.cs
+++ Proyecto_De_Consola/Program.cs
@@ -1,7 +1,27 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Hexagonal.Application.Case_Uso;
+using Hexagonal.Application.Port_Primary;
+using Hexagonal.Domain.interfaces;
+using Hexagonal.Infraestructure.DBContext;
+using Hexagonal.Infraestructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional:false,reloadOnChange:true)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .Build();
 
-string connectionString = configuration.GetConnectionString("DefaultConnection");
+string connectionString = configuration.GetConnectionString("DefaultConnection")!;
+
+//Inyector de dependecias
+var service = new ServiceCollection();
+service.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+//Implementaciones de los puertos
+service.AddTransient<IItemRepository, ItemRepository>();
+service.AddTransient<IItemService, ItemService>();
+
+//Creacion de objeto a partir de lo que inyectamos
+var serviceProvider = service.BuildServiceProvider();
+var itemsService = serviceProvider.GetRequiredService<IItemService>();
