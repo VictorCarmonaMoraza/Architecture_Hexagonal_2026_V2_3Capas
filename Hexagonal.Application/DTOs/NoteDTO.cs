e576ea70109cabc7646a349c7a109ed0337e5628
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-11 21:46:33 +0100

    Creacion de DTO en Capa Aplicacion

---
+++ Hexagonal.Application/DTOs/NoteDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Hexagonal.Application.DTOs
+{
+    public class NoteDTO
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public string Message { get; set; }
+
+        //El signo de interrogacion indica que el campo puede ser nulo
+        public string? Color { get; set; }
+    }
+}
