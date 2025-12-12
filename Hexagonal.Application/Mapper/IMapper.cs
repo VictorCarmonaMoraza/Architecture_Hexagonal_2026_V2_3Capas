b397b569ed3b87110100215f31c60efdfb63146b
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-12 13:55:36 +0100

    Creacion de Puerto Secundario IAddRepository

---
+++ Hexagonal.Application/Mapper/IMapper.cs
@@ -0,0 +1,7 @@
+﻿namespace Hexagonal.Application.Mapper
+{
+    public interface IMapper<Data_Entrada,Data_Salida>
+    {
+        public Data_Salida Map(Data_Entrada data_entrada);
+    }
+}
---
+++ Hexagonal.Domain/IRepository/IAddRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Hexagonal.Domain.IRepository
+{
+    //Algun modelo llegara pero no sabemos cual
+    public interface IAddRepository<TModel>
+    {
+        //Haremos una insercion asincrona
+        Task AddAsync(TModel model);
+    }
+}
