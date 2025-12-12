836b5d5fe8899a0bbf18c4e98dab07abd75fe11b
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-12 19:46:49 +0100

    Creacion de Adapatdor Secundario NoteMapperRepository

---
+++ Hexagonal.Infraestructure/Repository/NoteMapperRepository.cs
@@ -0,0 +1,22 @@
+﻿using Hexagonal.Domain.IRepository;
+using Hexagonal.Infraestructure.DBContext;
+using Hexagonal.Infraestructure.Models;
+
+namespace Hexagonal.Infraestructure.Repository
+{
+    public class NoteMapperRepository : IAddRepository<NoteModel>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NoteMapperRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(NoteModel model)
+        {
+            await _context.NotesModel.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
