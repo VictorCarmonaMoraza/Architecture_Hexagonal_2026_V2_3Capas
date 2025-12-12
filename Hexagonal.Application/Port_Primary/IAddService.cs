5ea38985e11184e2572441565957b1dd27caa93b
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-12 14:11:22 +0100

    Creacion de Puerto Primario IAddService

---
+++ Hexagonal.Application/Port_Primary/IAddService.cs
@@ -0,0 +1,9 @@
+﻿namespace Hexagonal.Application.Port_Primary
+{
+    //la entrada sera un DTO y la salida un modelo
+    public interface IAddService<Entrada_data_DTO,Salida_data_Modelo>
+    {
+        //Haremos una insercion asincrona
+        Task AddAsync(Entrada_data_DTO dto);
+    }
+}
